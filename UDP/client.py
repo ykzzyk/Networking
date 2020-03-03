ed76ef581c786b2eee21fb846db168e4a37db587
PumkinQ <nz9024295@gmail.com>
2020-03-02 22:31:06 -0600

    UDP

--- UDP/client.py
+++ UDP/client.py
@@ -1,14 +1,16 @@
-from socket import *
+import socket
 
-server_name = 'Server'
+server_name = '127.0.0.1'
 server_port = 12000
 
-clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-message = input("Input lowercase sentence")
+message = input("Input lowercase sentence:\n")
+message = message.encode()
 
 clientSocket.sendto(message, (server_name, server_port))
 
 modified_message, server_address = clientSocket.recvfrom(2048)
 
+print(f'modified_message: {modified_message}')
 clientSocket.close()
