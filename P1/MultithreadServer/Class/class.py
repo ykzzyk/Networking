e5d59270aad36f19d1f59b6c8569ea39d158a5a9
PumkinQ <nz9024295@gmail.com>
2020-03-10 18:21:32 -0500

    add class

---
+++ P1/MultithreadServer/Class/class.py
@@ -0,0 +1,14 @@
+class TCP():
+    def __init__(self, ip, port, bfsz):
+        self.ip = ip
+        self.port = port
+        self.bfsz = bfsz
+        
+    def tcp_server(self):
+        pass
+    
+    def tcp_client(self):
+        pass
+        
+        
+        
