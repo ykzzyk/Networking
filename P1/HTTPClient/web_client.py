f3bec88f40e9dd638d6c11d775c9e94272c283f9
PumkinQ <nz9024295@gmail.com>
2020-03-10 03:02:18 -0500

    add new files

---
+++ P1/HTTPClient/web_client.py
@@ -0,0 +1,41 @@
+from socket import *
+import argparse
+
+
+# Parameters
+#TCP_IP = '10.102.19.207'
+#TCP_PORT = 12003
+BUFFER_SIZE = 1024
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('server_host')
+parser.add_argument('server_port')
+parser.add_argument('filename')
+args = parser.parse_args()
+
+# Prepare a client socket
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((args.server_host, int(args.server_port)))
+
+# Send message to GET HTML file
+# Filename: HelloWorld.html
+MESSAGE = f'GET {args.filename}'
+MESSAGE = bytes(MESSAGE, 'utf-8')
+clientSocket.send(MESSAGE)
+
+# GET the full content from the HTML file
+full_content = ''
+while True:
+    data = clientSocket.recv(BUFFER_SIZE)
+    if not data:
+        break
+    data = data.decode('utf-8')
+    full_content += data
+    
+print("received data:", full_content)
+
+# Close Client
+print("\n\nClient close successfully!")
+clientSocket.close()
+
---
+++ P1/MultithreadServer/web_server.py
@@ -0,0 +1,56 @@
+#import socket module
+from socket import *
+
+# Parameters
+TCP_IP = '10.102.19.207'
+TCP_PORT = 12003
+BUFFER_SIZE = 1024
+
+# Prepare a server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+
+# Bind IP address and port
+serverSocket.bind((TCP_IP, TCP_PORT))
+
+#listening for  Client
+serverSocket.listen(1)
+print('Ready to serve..')
+serverSocket.settimeout(20)
+
+#Establish the connection
+connectionSocket, addr = serverSocket.accept()
+
+while True:
+    try:
+        # Receive the message from the Client
+        message = connectionSocket.recv(1024)
+        
+        filename = message.split()[1].decode('utf-8')
+        
+        # filename = 'HelloWorld.html'
+        with open(filename) as f:
+            outputdata = f.read()
+        
+        # Send one HTTP header line into socket
+        connectionSocket.send(b'HTTP/1.1 200 OK\r\n\r\n')
+
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(bytes(outputdata[i], 'utf-8'))
+        
+        #Close client socket
+        connectionSocket.close()
+        break
+        
+    except IOError:
+        #Send response message for file not found
+        connectionSocket.send(b'404 Not Found')
+        
+        #Close client socket
+        connectionSocket.close()
+        break
+
+# Close Server 
+print("\n\nServer Quit Successfully!")      
+serverSocket.close()
