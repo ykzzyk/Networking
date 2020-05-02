61fb823995d6e1a9eed79a3acccc59d220c9d1f0
PumkinQ <nz9024295@gmail.com>
2020-05-02 16:03:53 -0500

    changes

--- Final/draft.py
+++ Final/draft.py
@@ -13,7 +13,6 @@ import socket
 import threading
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-from tkinter.scrolledtext import ScrolledText
 import argparse
 import os
 
@@ -22,49 +21,35 @@ def recvMessage(socket):
    while True:
       try:
          msg = socket.recv(4096).decode('utf8')
-         ui_messages.insert(tk.END, f"{msg}\n")
+         msg_list.insert(tk.END, msg)
       except OSError:
          break
-      
-# Send message to server
-def sendMessage(flag = ""):
-        if flag == "Gotta go, TTYL!":
-            s.send(flag.encode('utf-8'))
-            s.close()
-            window.quit()
-        else:
-            # Get user input (minus newline character at end)
-            msg = ui_input.get("0.0", tk.END+"-1c")
 
-            print("UI: Got text: '%s'" % msg)
-
-            s.send(msg.encode('utf-8'))
-            
-            if msg == "Gotta go, TTYL!":
-                s.close()
-                window.quit()
-        
-        # Add this data to the message window
-        ui_messages.yview(tk.END)  # Auto-scrolling
-        
-        # Clean out input field for new data
-        ui_input.delete("0.0", tk.END)
+# Send message to the server
+def sendMessage(event=None):
+   msg = my_msg.get() 
+   my_msg.set("")
+   s.send(msg.encode('utf-8'))
+   if msg == "Gotta go, TTYL!":
+      s.close()
+      window.quit()
       
 # Send file to the server
-def sendFile():
+def sendFile(event=None):
    file = askopenfilename()
    if(len(file) > 0 and os.path.isfile(file)):
       print("UI: Selected file: %s" % file)
       with open(file, 'rb') as f:
-         filename = b'\n<sending ' + file.split('/')[-1].encode('utf-8') + b'>... \n'
+         filename = b'sending file name - ' + file.split('/')[-1].encode('utf-8') + b': '
          s.send(filename + f.read())
    else:
       print("UI: File operation canceled")
 
 
 # Close the window
-def hover_close():
-   sendMessage("Gotta go, TTYL!")
+def hover_close(event=None):
+   my_msg.set("Gotta go, TTYL!")
+   sendMessage()
 
 # Main funciton
 if __name__ == '__main__':
@@ -82,36 +67,32 @@ if __name__ == '__main__':
     
     # Use tkinter
     window = tk.Tk()
-    window.wm_title("ChatRoom/1.0 Connected to: "+ args.server_ip + "/"+str(args.server_port))
-    window.resizable('1','1')
+    window.title("ChatRoom/1.0 Connected to: "+ args.server_ip + ": "+str(args.server_port))
     
-    label = tk.Label(window, text = "TYPE <Gotta go, TTYL!> to QUIT", width = 45, font=("Arial Rounded MT Bold", 12), fg="grey", anchor="w")
+    messages_frame = tk.Frame(window)
+    label = tk.Label(window, text = "TYPE <Gotta go, TTYL!> to QUIT", width = 53, font=("Helvetica", 12), fg="Blue", anchor="w")
     label.pack()
+    
+    my_msg = tk.StringVar()
     
     s.send(args.username.encode('utf-8'))
     
-    ui_messages = ScrolledText(
-            window,
-            wrap=tk.WORD,
-            width=50,  # In chars
-            height=25)  # In chars
-    ui_messages.pack(side=tk.TOP, fill=tk.BOTH)
     
-    
-    ui_input = tk.Text(
-            window,
-            wrap=tk.WORD,
-            width=50,
-            height=4)
-    ui_input.insert(tk.END, "<Enter message>")
-    ui_input.bind("<Return>", sendMessage)
-    ui_input.pack(side=tk.TOP, fill=tk.BOTH)
-    
-    file_button = tk.Button(window, text="File", command=sendFile, width = 5)
-    file_button.pack(side=tk.RIGHT)
-    
+    scrollbar = tk.Scrollbar(messages_frame)  
+    msg_list = tk.Listbox(messages_frame, height=25, width=40, yscrollcommand=scrollbar.set)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    msg_list.pack(side=tk.LEFT, fill=tk.BOTH)
+    msg_list.pack()
+    messages_frame.pack()
+
+    entry_field = tk.Entry(window, textvariable=my_msg, width = 30)
+    entry_field.bind("<Return>", sendMessage)
+    entry_field.pack(side=tk.LEFT)
     send_button = tk.Button(window, text="Send", command=sendMessage, width=5)
     send_button.pack(side=tk.LEFT)
+    
+    file_button = tk.Button(window, text="File", command=sendFile, width = 5)
+    file_button.pack(side=tk.LEFT)
 
     window.protocol("WM_DELETE_WINDOW", hover_close)
 
